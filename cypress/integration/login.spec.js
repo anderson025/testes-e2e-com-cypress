0841e378391a10fcc4b1f818efb5cdbf53e15065
Anderson Silva <sistemas.anderson@hotmail.com>
2023-02-11 13:42:00 -0300

    Create test cypress

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,11 @@
+it('successfully logs in', () => {
+    cy.intercept('GET', '**/notes').as('getNotes')
+  
+    cy.login(
+      Cypress.env('USER_EMAIL'),
+      Cypress.env('USER_PASSWORD'),
+      
+    )
+  
+    cy.wait('@getNotes')
+})
