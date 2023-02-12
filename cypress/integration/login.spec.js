cb53d550c68c78d32e684769b60b9c276498b9c9
Anderson Silva <sistemas.anderson@hotmail.com>
2023-02-12 14:07:11 -0300

    ajustes no lint

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -4,7 +4,7 @@ it('successfully logs in', () => {
   cy.login(
     Cypress.env('USER_EMAIL'),
     Cypress.env('USER_PASSWORD'),
-    { cacheSession: false } 
+    { cacheSession: false }
   )
 
   cy.wait('@getNotes')
