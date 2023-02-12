6c18cc1c36c815dddd9a6874fe409f2e9a1fa08c
Anderson Silva <sistemas.anderson@hotmail.com>
2023-02-12 13:08:38 -0300

    ajustes lint

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,32 +1,32 @@
 Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
-    cy.visit('/signup')
-    cy.get('#email').type(email)
-    cy.get('#password').type(password, { log: false })
-    cy.get('#confirmPassword').type(password, { log: false })
-    cy.contains('button', 'Signup').click()
-    cy.get('#confirmationCode').should('be.visible')
+  cy.visit('/signup')
+  cy.get('#email').type(email)
+  cy.get('#password').type(password, { log: false })
+  cy.get('#confirmPassword').type(password, { log: false })
+  cy.contains('button', 'Signup').click()
+  cy.get('#confirmationCode').should('be.visible')
 })
 
 
 Cypress.Commands.add('login', (
-    username = Cypress.env('USER_EMAIL'),
-    password = Cypress.env('USER_PASSWORD'),
-    { cacheSession = true } = {}
-  ) => {
-    const login = () => {
-      cy.visit('/login')
-      cy.get('#email').type(username)
-      cy.get('#password').type(password, { log: false })
-      cy.contains('button', 'Login').click()
-      cy.contains('h1', 'Your Notes').should('be.visible')
-    }
-  
-    if (cacheSession) {
-      cy.session([username, password], login)
-    } else {
-      login()
-    }
-  })
+  username = Cypress.env('USER_EMAIL'),
+  password = Cypress.env('USER_PASSWORD'),
+  { cacheSession = true } = {}
+) => {
+  const login = () => {
+    cy.visit('/login')
+    cy.get('#email').type(username)
+    cy.get('#password').type(password, { log: false })
+    cy.contains('button', 'Login').click()
+    cy.contains('h1', 'Your Notes').should('be.visible')
+  }
+
+  if (cacheSession) {
+    cy.session([username, password], login)
+  } else {
+    login()
+  }
+})
 
 
 const attachFileHandler = () => cy.get('#file').attachFile('example.json')
@@ -72,21 +72,21 @@ Cypress.Commands.add('deleteNote', note => {
 })
 
 Cypress.Commands.add('fillSettingsFormAndSubmit', () => {
-    cy.visit('/settings')
-    cy.get('#storage').type('1')
-    cy.get('#name').type('Mary Doe')
-    cy.iframe('.card-field iframe')
-      .as('iframe')
-      .find('[name="cardnumber"]')
-      .type('4242424242424242')
-    cy.get('@iframe')
-      .find('[name="exp-date"]')
-      .type('1271')
-    cy.get('@iframe')
-      .find('[name="cvc"]')
-      .type('123')
-    cy.get('@iframe')
-      .find('[name="postal"]')
-      .type('12345')
-    cy.contains('button', 'Purchase').click()
-  })
+  cy.visit('/settings')
+  cy.get('#storage').type('1')
+  cy.get('#name').type('Mary Doe')
+  cy.iframe('.card-field iframe')
+    .as('iframe')
+    .find('[name="cardnumber"]')
+    .type('4242424242424242')
+  cy.get('@iframe')
+    .find('[name="exp-date"]')
+    .type('1271')
+  cy.get('@iframe')
+    .find('[name="cvc"]')
+    .type('123')
+  cy.get('@iframe')
+    .find('[name="postal"]')
+    .type('12345')
+  cy.contains('button', 'Purchase').click()
+})
